8ee33f161760d0fa8cb6e3260e9f1e795859db54
chris martinex <uschrismartinez@gmail.com>
2023-03-24 15:37:57 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -10,4 +10,7 @@ export default function App() {
   );
 }
 
+
+// Peer reviewed by Aisha
+// This app works well with the icons but it’s not showing the rest of the data .I think if u took more time on it you would be able to get it because u basically got it the first part working. Other than that the job on the design.your code was clean and well put together.also u needed to work on the mobile responsiveness.
  
